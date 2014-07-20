9068abe98a821df82d1a2e7521d0d806d5938f7b
tong <tong@disktree.net>
2014-07-20 19:21:21 +0200

    Remove CubeGeometry (three.js depricated)

--- src/js/three/CubeGeometry.hx
+++
@@ -1,6 +0,0 @@
-package three;
-
-@:native("THREE.CubeGeometry")
-extern class CubeGeometry extends Geometry {
-    function new(width:Float, height:Float, depth:Float, ?widthSegments:Float, ?heightSegments:Float, ?depthSegments:Float) : Void;
-}
