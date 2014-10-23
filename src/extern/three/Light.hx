5d0e8ef0a716ee36a903d20e3790ab226531ff3f
tong <tong@disktree.net>
2014-10-23 13:07:27 +0200

    Added abstract enum for light types

--- src/extern/three/Light.hx
+++ src/extern/three/Light.hx
@@ -1,8 +1,17 @@
 package three;
 
+@:enum abstract LightType(String) {
+    var AmbientLight = "AmbientLight";
+    var AreaLight = "AreaLight";
+    var DirectionalLight = "DirectionalLight";
+    var HemisphereLight = "HemisphereLight";
+    var SpotLight = "SpotLight";
+}
+
 @:native("THREE.Light")
 extern class Light extends Object3D {
     var color : Color;
+    var type(default,null) : LightType;
     function new( hexColor : Int ) : Void;
     // override function clone(?light:Light) : Light;
 }
