d46c13578d0f402ddeae0cbf22ebf465cb6c9356
tong <tong@disktree.net>
2014-11-23 00:26:35 +0100

    Fix

--- src/extern/three/SpotLightHelper.hx
+++ src/extern/three/SpotLightHelper.hx
@@ -5,7 +5,7 @@ extern class SpotLightHelper extends Object3D {
 	var lightSphere : Mesh;
 	var light : SpotLight;
 	var lightCone : Mesh;
-	function new( light : SpotLight ) : Void;
+	function new( light : SpotLight, ?sphereSize : Float ) : Void;
 	function update() : Void;
 	function dispose() : Void;
 }
