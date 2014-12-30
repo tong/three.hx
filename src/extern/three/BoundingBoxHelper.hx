b7cf69ca22755b28b1caf03b5ffd4b00562104e9
tong <tong@disktree.net>
2014-12-30 20:29:57 +0100

    Fix

--- src/extern/three/BoundingBoxHelper.hx
+++ src/extern/three/BoundingBoxHelper.hx
@@ -5,4 +5,5 @@ extern class BoundingBoxHelper extends Mesh {
 	var object : Mesh;
 	var box : Box3;
 	function new( object : Mesh, ?hex : Int ) : Void;
+	function update() : Void;
 }
--- src/extern/three/BoxHelper.hx
+++
@@ -1,7 +0,0 @@
-package three;
-
-@:native("THREE.BoxHelper")
-extern class BoxHelper extends Line {
-	function new( object : Object3D ) : Void;
-	function update( object : Object3D ) : Void;
-}
