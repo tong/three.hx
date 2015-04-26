4ad91cc7f4caf4d2e0f1b96f0ce5de6aca124f8d
tong <tong@disktree.net>
2015-04-26 17:24:01 +0200

    Fix

--- src/three/Bone.hx
+++ src/three/Bone.hx
@@ -2,8 +2,8 @@ package three;
 
 @:native("THREE.Bone")
 extern class Bone extends Object3D {
-	var skin : Bool; // belongToSkin
+	var skin : SkinnedMesh;
 	var skinMatrix : Matrix4;
-	function new( belongToSkin : Bool ) : Void;
+	function new( belongToSkin : SkinnedMesh ) : Void;
 	function update( parentSkinMatrix : Matrix4, ?forceUpdate : Bool ) : Void;
 }
--- src/three/SkinnedMesh.hx
+++ src/three/SkinnedMesh.hx
@@ -2,13 +2,15 @@ package three;
 
 @:native("THREE.SkinnedMesh")
 extern class SkinnedMesh extends Mesh {
-    var useVertexTexture : Bool; // true
+    var useVertexTexture : Bool;
     var identityMatrix : Matrix4;
     var bones : Array<Bone>;
     var bonesMatrices : Array<Float>;
-    function new(geometry:Geometry, ?material:Material, ?useVertexTexture:Bool) : Void;
-    function addBone(bone:Bone) : Bone;
-    //override function updateMatrixWorld(force:Bool) : Void;
+    var skeleton : Skeleton;
+    function new( geometry : Geometry, ?material : Material, ?useVertexTexture : Bool ) : Void;
+    function addBone( bone : Bone ) : Bone;
+    function bind( skeleton : Skeleton, bindMatrix : Matrix4 ) : Void;
     function pose() : Void;
+    function normalizeSkinWeights() : Void;
     // override function clone(?object:SkinnedMesh) : SkinnedMesh;
 }
