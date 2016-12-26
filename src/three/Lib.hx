edc87d0dece9a829f8d86e73778777ed23d17515
tong <tong@disktree.net>
2016-12-26 06:20:04 +0100

    Update typedef remaps

--- src/three/Lib.hx
+++ src/three/Lib.hx
@@ -1,25 +1,51 @@
 package three;
 
-////////////////////////////////////////////////////////////////////////////////
-
-//@:noDoc typedef Three = three.Three;
-//@:noDoc typedef Three = three.Three;
-
+@:noDoc typedef AnimationAction = three.animation.AnimationAction;
 @:noDoc typedef AnimationClip = three.animation.AnimationClip;
 @:noDoc typedef AnimationMixer = three.animation.AnimationMixer;
 
+@:noDoc typedef Audio = three.audio.Audio;
+@:noDoc typedef AudioAnalyser = three.audio.AudioAnalyser;
+@:noDoc typedef AudioBuffer = three.audio.AudioBuffer;
+@:noDoc typedef AudioListener = three.audio.AudioListener;
+@:noDoc typedef PositionalAudio = three.audio.PositionalAudio;
+
 @:noDoc typedef Camera = three.cameras.Camera;
+@:noDoc typedef CubeCamera = three.cameras.CubeCamera;
+@:noDoc typedef OrthographicCamera = three.cameras.OrthographicCamera;
 @:noDoc typedef PerspectiveCamera = three.cameras.PerspectiveCamera;
+@:noDoc typedef StereoCamera = three.cameras.StereoCamera;
 
+@:noDoc typedef BufferAttribute = three.core.BufferAttribute;
+@:noDoc typedef BufferGeometry = three.core.BufferGeometry;
+@:noDoc typedef Clock = three.core.Clock;
+@:noDoc typedef CurvePath = three.core.CurvePath;
+@:noDoc typedef DirectGeometry = three.core.DirectGeometry;
+@:noDoc typedef EventDispatcher = three.core.EventDispatcher;
+@:noDoc typedef Geometry = three.core.Geometry;
+@:noDoc typedef InstancedBufferAttribute = three.core.InstancedBufferAttribute;
+@:noDoc typedef InstancedBufferGeometry = three.core.InstancedBufferGeometry;
+@:noDoc typedef InstancedInterleavedBuffer = three.core.InstancedInterleavedBuffer;
+@:noDoc typedef InterleavedBuffer = three.core.InterleavedBuffer;
+@:noDoc typedef InterleavedBufferAttribute = three.core.InterleavedBufferAttribute;
+@:noDoc typedef Layers = three.core.Layers;
 @:noDoc typedef Object3D = three.core.Object3D;
+@:noDoc typedef Path = three.core.Path;
+@:noDoc typedef Ray = three.core.Ray;
+@:noDoc typedef Raycaster = three.core.Raycaster;
+@:noDoc typedef UV = three.core.UV;
 
+@:noDoc typedef CurveUtils = three.extras.CurveUtils;
 @:noDoc typedef SceneUtils = three.extras.SceneUtils;
+@:noDoc typedef ShapeUtils = three.extras.ShapeUtils;
+
 @:noDoc typedef AxisHelper = three.extras.helpers.AxisHelper;
 @:noDoc typedef BoundingBoxHelper = three.extras.helpers.BoundingBoxHelper;
 //@:noDoc typedef BoxHelper = three.extras.helpers.BoxHelper;
 @:noDoc typedef CameraHelper = three.extras.helpers.CameraHelper;
 @:noDoc typedef DirectionalLightHelper = three.extras.helpers.DirectionalLightHelper;
 @:noDoc typedef GridHelper = three.extras.helpers.GridHelper;
+@:noDoc typedef PointLightHelper = three.extras.helpers.PointLightHelper;
 
 @:noDoc typedef BoxGeometry = three.geometries.BoxGeometry;
 @:noDoc typedef CircleBufferGeometry = three.geometries.CircleBufferGeometry;
@@ -44,16 +70,28 @@ package three;
 
 @:noDoc typedef AmbientLight = three.lights.AmbientLight;
 @:noDoc typedef DirectionalLight = three.lights.DirectionalLight;
+@:noDoc typedef DirectionalLightShadow = three.lights.DirectionalLightShadow;
+@:noDoc typedef HemisphereLight = three.lights.HemisphereLight;
 @:noDoc typedef Light = three.lights.Light;
+@:noDoc typedef LightShadow = three.lights.LightShadow;
 @:noDoc typedef PointLight = three.lights.PointLight;
 @:noDoc typedef SpotLight = three.lights.SpotLight;
 
 @:noDoc typedef JSONLoader = three.loaders.JSONLoader;
+@:noDoc typedef Loader = three.loaders.Loader;
+@:noDoc typedef ObjectLoader = three.loaders.ObjectLoader;
 
+@:noDoc typedef LineBasicMaterial = three.materials.LineBasicMaterial;
+@:noDoc typedef LineDashedMaterial = three.materials.LineDashedMaterial;
 @:noDoc typedef Material = three.materials.Material;
 @:noDoc typedef MeshBasicMaterial = three.materials.MeshBasicMaterial;
-@:noDoc typedef MeshPhongMaterial = three.materials.MeshPhongMaterial;
+@:noDoc typedef MeshDepthMaterial = three.materials.MeshDepthMaterial;
 @:noDoc typedef MeshLambertMaterial = three.materials.MeshLambertMaterial;
+@:noDoc typedef MeshNormalMaterial = three.materials.MeshNormalMaterial;
+@:noDoc typedef MeshPhongMaterial = three.materials.MeshPhongMaterial;
+@:noDoc typedef RawShaderMaterial = three.materials.RawShaderMaterial;
+@:noDoc typedef ShaderMaterial = three.materials.ShaderMaterial;
+@:noDoc typedef SpriteMaterial = three.materials.SpriteMaterial;
 
 @:noDoc typedef Box2 = three.math.Box2;
 @:noDoc typedef Box3 = three.math.Box3;
@@ -65,7 +103,7 @@ package three;
 @:noDoc typedef Matrix4 = three.math.Matrix4;
 @:noDoc typedef Plane = three.math.Plane;
 @:noDoc typedef Quaternion = three.math.Quaternion;
-@:noDoc typedef Ray = three.math.Ray;
+//@:noDoc typedef Ray = three.math.Ray;
 @:noDoc typedef Sphere = three.math.Sphere;
 @:noDoc typedef Spline = three.math.Spline;
 @:noDoc typedef Triangle = three.math.Triangle;
@@ -73,16 +111,33 @@ package three;
 @:noDoc typedef Vector3 = three.math.Vector3;
 @:noDoc typedef Vector4 = three.math.Vector4;
 
+@:noDoc typedef Bone = three.objects.Bone;
+@:noDoc typedef Group = three.objects.Group;
+@:noDoc typedef LensFlare = three.objects.LensFlare;
 @:noDoc typedef Line = three.objects.Line;
+@:noDoc typedef LineSegments = three.objects.LineSegments;
+@:noDoc typedef LOD = three.objects.LOD;
 @:noDoc typedef Mesh = three.objects.Mesh;
+@:noDoc typedef Skeleton = three.objects.Skeleton;
+@:noDoc typedef SkinnedMesh = three.objects.SkinnedMesh;
+@:noDoc typedef Sprite = three.objects.Sprite;
 
 @:noDoc typedef Renderer = three.renderers.Renderer;
 @:noDoc typedef WebGLRenderer = three.renderers.WebGLRenderer;
+@:noDoc typedef WebGLRenderTarget = three.renderers.WebGLRenderTarget;
+@:noDoc typedef WebGLRenderTargetCube = three.renderers.WebGLRenderTargetCube;
 
+@:noDoc typedef Fog = three.scenes.Fog;
+@:noDoc typedef FogExp2 = three.scenes.FogExp2;
+@:noDoc typedef IFog = three.scenes.IFog;
 @:noDoc typedef Scene = three.scenes.Scene;
 
-//.....
-
+@:noDoc typedef CanvasTexture = three.textures.CanvasTexture;
+@:noDoc typedef CompressedTexture = three.textures.CompressedTexture;
+@:noDoc typedef CubeTexture = three.textures.CubeTexture;
+@:noDoc typedef DataTexture = three.textures.DataTexture;
+@:noDoc typedef Texture = three.textures.Texture;
+@:noDoc typedef VideoTexture = three.textures.VideoTexture;
 
 
 ////////////////////////////////////////////////////////////////////////////////
