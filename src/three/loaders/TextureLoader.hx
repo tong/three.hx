90767433ae8f9218432d81ba1253af1db6d5de6f
tong <tong@disktree.net>
2017-04-10 19:25:57 +0200

    Add TextureLoader

---
+++ src/three/loaders/TextureLoader.hx
@@ -0,0 +1,11 @@
+package three.loaders;
+
+import three.textures.Texture;
+
+@:native("THREE.TextureLoader")
+extern class TextureLoader  {
+    function new( ?manager : Dynamic ) : Void;
+    function load( url : String, ?onLoad : Dynamic->Void, ?onProgress : Dynamic->Void, ?onError : Dynamic->Void ) : Texture;
+    function setCrossOrigin( value : Bool ) : Void;
+    function setPath( value : String ) : Void;
+}
