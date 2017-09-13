239f67135f74f4b1e28fed5ff3d8f59fa00d0df5
tong <tong@disktree.net>
2017-09-13 14:03:40 +0200

    Fix

--- src/three/extras/core/Shape.hx
+++ src/three/extras/core/Shape.hx
@@ -11,13 +11,13 @@ extern class Shape extends three.core.Path {
     var holes : Array<Dynamic>;
     //var holes : Array<Vector2>;
 
-    function new( ?points : Array<Vector2> ) : Void; //?
+    function new( ?points : Array<Vector2> ) : Void;
 
-    function extrude( options : Dynamic ) : ExtrudeGeometry;
-    function makeGeometry( options : Dynamic ) : ShapeGeometry;
+    //function extrude( options : Dynamic ) : ExtrudeGeometry;
+    //function makeGeometry( options : Dynamic ) : ShapeGeometry;
     function getPointsHoles( divisions : Int ) : Array<Vector2>;
-    function getSpacedPointsHoles( divisions : Int ) : Array<Vector2>;
     function extractAllPoints( divisions : Int ) : { shape:Array<Vector2>, holes:Array<Vector2> };
     function extractPoints( divisions : Int ) : { shape:Array<Vector2>, holes:Array<Vector2> };
-    function extractAllSpacedPoints( divisions : Int ) : { shape:Array<Vector2>, holes:Array<Vector2> };
+    //function getSpacedPointsHoles( divisions : Int ) : Array<Vector2>;
+    //function extractAllSpacedPoints( divisions : Int ) : { shape:Array<Vector2>, holes:Array<Vector2> };
 }
