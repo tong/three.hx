859f62a0ddafb046899032b614dd628fa409cb3a
tong <tong@disktree.net>
2017-12-03 23:01:36 +0100

    Cleanup

---
+++ src/three/extras/EarCut.hx
@@ -0,0 +1,7 @@
+package three.extras;
+
+@:native("THREE.EarCut")
+extern class EarCut {
+    //TODO
+    static function triangulate( data : Array<Dynamic>, holeIndices : Dynamic, dim : Dynamic ) : Array<Dynamic>;
+}
