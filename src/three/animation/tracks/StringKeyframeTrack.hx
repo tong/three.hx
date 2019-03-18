05cac726fd920dce0c16ab0dff5ca587c54db2f2
tong <tong@disktree.net>
2019-03-18 01:24:42 +0100

    Fix

--- src/three/animation/tracks/StringKeyframeTrack.hx
+++ src/three/animation/tracks/StringKeyframeTrack.hx
@@ -1,5 +1,7 @@
 package three.animation.tracks;
 
+import three.Lib.InterpolationModes;
+
 @:native("THREE.StringKeyframeTrack")
 extern class StringKeyframeTrack extends three.animation.KeyframeTrack {
 	function new( name : String, times : Array<Dynamic>, values : Array<Dynamic>, ?interpolation : InterpolationModes ) : Void;
--- src/three/animation/tracks/VectorKeyframeTrack.hx
+++ src/three/animation/tracks/VectorKeyframeTrack.hx
@@ -1,5 +1,7 @@
 package three.animation.tracks;
 
+import three.Lib.InterpolationModes;
+
 @:native("THREE.VectorKeyframeTrack")
 extern class VectorKeyframeTrack extends three.animation.KeyframeTrack {
 	function new( name : String, times : Array<Dynamic>, values : Array<Dynamic>, ?interpolation : InterpolationModes ) : Void;
