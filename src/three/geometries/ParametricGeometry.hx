94b1aa83c234fd5678ad28b03393fdeb19de68aa
tong <tong@disktree.net>
2019-04-11 09:43:34 +0200

    Fix

--- src/three/geometries/ParametricGeometry.hx
+++ src/three/geometries/ParametricGeometry.hx
@@ -1,6 +1,8 @@
 package three.geometries;
 
+import three.math.Vector3;
+
 @:native("THREE.ParametricGeometry")
 extern class ParametricGeometry extends three.core.Geometry {
-	function new( func : Dynamic, slices : Int, stacks : Int ) : Void;
+	function new( func : Float->Float->Vector3->Void, slices : Int, stacks : Int ) : Void;
 }
