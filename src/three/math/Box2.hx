8ad5c2c8e8a33cce042d1ca024b14c7bb1031a24
tong <tong@disktree.net>
2019-04-11 09:04:38 +0200

    Fix

--- src/three/math/Box2.hx
+++ src/three/math/Box2.hx
@@ -2,29 +2,29 @@ package three.math;
 
 @:native("THREE.Box2")
 extern class Box2 {
-    var min : Vector3; // (Infinity, Infinity)
-    var max : Vector3; // (-Infinity, -Infinity)
+    var min : Vector2;
+    var max : Vector2;
     function new( ?min : Vector2, ?max : Vector2 ) : Void;
-    function set(min:Vector2, max:Vector2) : Box2; // this
-    function setFromPoints(points:Array<Vector2>) : Box2; // this
-    function setFromCenterAndSize(center:Vector2, size:Vector2) : Box2; // this
-    function copy(box:Box2) : Box2; // this
-    function makeEmpty() : Box2; // this
-    function empty() : Bool;
-    function center(?optionalTarget:Vector2) : Vector2;
-    function size(?optionalTarget:Vector2) : Vector2;
-    function expandByPoint(point:Vector2) : Box2; // this
-    function expandByVector(point:Vector2) : Box2; // this
-    function expandByScalar(scalar:Float) : Box2; // this
-    function containsPoint(point:Vector2) : Bool;
-    function containsBox(box:Box2) : Bool;
-    function getParameter(point:Vector2) : Vector2;
-    function isIntersectionBox(box:Box2) : Bool;
-    function clampPoint(point:Vector2, ?optionalTarget:Vector2) : Vector2;
-    function distanceToPoint(point:Vector2) : Float;
-    function intersect(box:Box2) : Box2; // this
-    function union(box:Box2) : Box2; // this
-    function translate(offset:Vector2) : Box2; // this
-    function equals(box:Box2) : Bool;
+    function set( min : Vector2, max : Vector2 ) : Box2;
+    function setFromPoints( points : Array<Vector2> ) : Box2;
+    function setFromCenterAndSize( center : Vector2, size : Vector2 ) : Box2;
     function clone() : Box2;
+    function copy( box : Box2 ) : Box2;
+    function makeEmpty() : Box2;
+    function isEmpty() : Bool;
+    function getCenter() : Vector2;
+    function getSize() : Vector2;
+    function expandByPoint( point : Vector2 ) : Box2;
+    function expandByVector( point : Vector2 ) : Box2;
+    function expandByScalar( scalar : Float ) : Box2;
+    function containsPoint( point : Vector2 ) : Bool;
+    function containsBox( box : Box2 ) : Bool;
+    function getParameter( point : Vector2 ) : Vector2;
+    function intersectsBox( box : Box2 ) : Bool;
+    function clampPoint( point : Vector2, ?target : Vector2 ) : Vector2;
+    function distanceToPoint( point : Vector2 ) : Float;
+    function intersect( box : Box2 ) : Box2;
+    function union( box : Box2 ) : Box2;
+    function translate( offset : Vector2 ) : Box2;
+    function equals( box : Box2 ) : Bool;
 }
