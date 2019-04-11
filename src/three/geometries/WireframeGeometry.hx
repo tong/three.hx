3b3f0b4bdb674102d50784452ecde38954bd973e
tong <tong@disktree.net>
2019-04-11 10:45:24 +0200

    Fix

--- src/three/geometries/WireframeGeometry.hx
+++ src/three/geometries/WireframeGeometry.hx
@@ -1,8 +1,10 @@
 package three.geometries;
 
+import haxe.extern.EitherType;
+import three.core.BufferGeometry;
 import three.core.Geometry;
 
 @:native("THREE.WireframeGeometry")
 extern class WireframeGeometry extends three.core.BufferGeometry {
-	function new( ?geometry : Geometry ) : Void;
+	function new( ?geometry : EitherType<Geometry,BufferGeometry> ) : Void;
 }
