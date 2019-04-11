ed0fac3a29657bcd344600b17f4105c57b8e8fff
tong <tong@disktree.net>
2019-04-11 09:36:45 +0200

    Remove BufferGeometries

--- src/three/geometries/CircleBufferGeometry.hx
+++
@@ -1,7 +0,0 @@
-package three.geometries;
-
-@:native("THREE.CircleBufferGeometry")
-extern class CircleBufferGeometry extends three.core.BufferGeometry {
-	function new( ?radius : Float, ?segments : Int, ?thetaStart : Float, ?thetaLength : Float ) : Void;
-    //function clone() : CircleBufferGeometry;
-}
--- src/three/geometries/TetrahedronBufferGeometry.hx
+++
@@ -1,6 +0,0 @@
-package three.geometries;
-
-@:native("THREE.TetrahedronBufferGeometry")
-extern class TetrahedronBufferGeometry extends PolyhedronGeometry {
-    function new( radius : Float, ?detail : Int ) : Void;
-}
