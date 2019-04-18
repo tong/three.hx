f85094f43ad57d68e055e0ff258c7ef56eddca96
tong <tong@disktree.net>
2019-04-18 09:59:16 +0200

    Fix

--- src/three/geometries/PlaneBufferGeometry.hx
+++ src/three/geometries/PlaneBufferGeometry.hx
@@ -2,5 +2,5 @@ package three.geometries;
 
 @:native("THREE.PlaneBufferGeometry")
 extern class PlaneBufferGeometry extends three.core.BufferGeometry {
-	function new( width : Float, height : Float, ?widthSegments : Float, ?heightSegments : Float) : Void;
+	function new( width : Float, height : Float, ?widthSegments : Int, ?heightSegments : Int ) : Void;
 }
--- src/three/geometries/PlaneGeometry.hx
+++ src/three/geometries/PlaneGeometry.hx
@@ -2,5 +2,5 @@ package three.geometries;
 
 @:native("THREE.PlaneGeometry")
 extern class PlaneGeometry extends three.core.Geometry {
-	function new( width : Float, height : Float, ?widthSegments : Float, ?heightSegments : Float) : Void;
+	function new( width : Float, height : Float, ?widthSegments : Int, ?heightSegments : Int ) : Void;
 }
