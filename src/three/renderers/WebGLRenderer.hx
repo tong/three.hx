2ffad2d7c2adfb4ca69a142caab63e491bc25262
tong <tong@disktree.net>
2019-12-21 20:36:28 +0100

    improve extern

--- src/three/renderers/WebGLRenderer.hx
+++ src/three/renderers/WebGLRenderer.hx
@@ -21,10 +21,16 @@ import three.scenes.Fog;
 import three.scenes.Scene;
 import three.textures.Texture;
 
-@:enum abstract RenderPrecision(String) from String to String{
+@:enum abstract RenderPrecision(String) to String {
     var highp = "highp";
     var mediump = "mediump";
     var lowp = "lowp";
+}
+
+@:enum abstract PowerPreference(String) to String {
+    var high_performance = "high-performance";
+    var low_power = "low-power";
+    var default_ = "default";
 }
 
 typedef RenderInfo = {
@@ -36,15 +42,16 @@ typedef RenderInfo = {
 }
 
 typedef WebGLRendererParameters = {
-    ?canvas : CanvasElement,
+    ?canvas : CanvasElement, //TODO: EitherType<CanvasElement,OffscreenCanvas>
     ?context : js.html.webgl.RenderingContext,
     ?precision : RenderPrecision,
     ?alpha : Bool,
-    ?depth : Bool,
-    ?stencil : Bool,
-    ?antialias : Bool,
     ?premultipliedAlpha : Bool,
+    ?antialias : Bool,
+    ?stencil : Bool,
     ?preserveDrawingBuffer : Bool,
+	?powerPreference : PowerPreference,
+    ?depth : Bool,
     ?logarithmicDepthBuffer : Bool,
 	?failIfMajorPerformanceCaveat : Bool
 }
@@ -54,10 +61,6 @@ extern class WebGLRenderer implements Renderer {
 
     var domElement : CanvasElement;
     var context : RenderingContext;
-    //var precision : RenderPrecision;
-    //var alpha : Float;
-
-    var devicePixelRatio : Float;
 
     var autoClear : Bool;
     var autoClearColor : Bool;
@@ -66,13 +69,18 @@ extern class WebGLRenderer implements Renderer {
 
     var sortObjects : Bool;
 
-    var autoUpdateObjects : Bool;
-    var autoUpdateScene : Bool;
+	var clippingPlanes : Array<Dynamic>; //TODO
+	var localClippingEnabled : Bool;
 
     var gammaFactor : Float;
-    var gammaInput : Bool;
-    var gammaOutput : Bool;
-    //var physicallyBasedShading : Bool;
+
+	var physicallyCorrectLight : Bool;
+
+	var outputEncoding : TextureEncoding;
+
+	var toneMapping : ToneMapping;
+	var toneMappingExposure : Float;
+	var toneMappingWhitePoint : Float;
 
     var maxMorphTargets : Int; // 8
     var maxMorphNormals : Int; // 4
@@ -85,9 +93,6 @@ extern class WebGLRenderer implements Renderer {
 
     var vr(default,null) : WebVRManager;
 
-    var renderPluginsPre : Array<Dynamic>; // TODO
-    var renderPluginsPost : Array<Dynamic>; // TODO
-
     var info : RenderInfo;
 
     var extensions : WebGLExtensions;
@@ -98,23 +103,13 @@ extern class WebGLRenderer implements Renderer {
     function getContext() : js.html.webgl.RenderingContext;
     function getContextAttributes() : js.html.webgl.ContextAttributes;
     function forceContextLoss() : Void;
-
-    function supportsVertexTextures() : Bool;
-    //function supportsFloatTextures() : Bool;
-    //function supportsStandardDerivatives() : Bool;
-    //function supportsCompressedTextureS3TC() : Bool;
-    //function supportsCompressedTextureS3TC() : Bool;
-
-    //function getMaxAnisotropy() : Int;
-    //function getPrecision() : RenderPrecision;
+    function forceContextRestore() : Void;
     function getPixelRatio() : Float;
     function setPixelRatio( value : Float ) : Void;
-    
-	function getDrawingBufferSize( target : Vector2 ) : Vector2;
-	function setDrawingBufferSize( width : Float, height : Float, pixelRatio : Float ) : Void;
-
     function getSize() : { width : Float, height : Float };
     function setSize(width:Int, height:Int) : Void;
+	function getDrawingBufferSize( target : Vector2 ) : Vector2;
+	function setDrawingBufferSize( width : Float, height : Float, pixelRatio : Float ) : Void;
     function getCurrentViewport( target : Vector4 ) : Vector4;
     function getViewport( target : Vector4 ) : Vector4;
     function setViewport( x : EitherType<Float,Vector4>, y : Float, width : Float, height : Float ) : Void;
@@ -122,30 +117,30 @@ extern class WebGLRenderer implements Renderer {
     function setScissor(x:Float, y:Float, width:Float, height:Float) : Void;
     function getScissorTest() : Bool;
     function setScissorTest( enable : Bool ) : Void;
-    function enableScissorTest(enable:Bool) : Void;
-
-    function setClearColor( color : EitherType<Color,EitherType<String,Float>>, ?alpha : Float ) : Void;
+    function setOpaqueSort( method : Dynamic ) : Void;
+    function setTransparentSort( method : Dynamic ) : Void;
     function getClearColor() : Color;
+    function setClearColor( color : EitherType<Color,EitherType<String,Float>>, ?alpha : Float ) : Void;
     function getClearAlpha() : Float;
     function setClearAlpha( alpha : Float ) : Void;
     function clear( ?color : Bool, ?depth : Bool, ?stencil : Bool ) : Void;
+    function clearColor() : Void;
     function clearDepth() : Void;
     function clearStencil() : Void;
-    function clearTarget( renderTarget : WebGLRenderTarget, color : Bool, depth : Bool, stencil : Bool ) : Void;
-	
     function dispose() : Void;
 
-    function addPostPlugin(plugin:Dynamic) : Void; // TODO
-    function addPrePlugin(plugin:Dynamic) : Void; // TODO
-    function updateShadowMap(scene:Scene, camera:Camera) : Void;
+    //function addPostPlugin(plugin:Dynamic) : Void; // TODO
+    //function addPrePlugin(plugin:Dynamic) : Void; // TODO
+    //function updateShadowMap(scene:Scene, camera:Camera) : Void;
 
-    function createParticleBuffers(geometry:Geometry) : Void;
-    function createLineBuffers(geometry:Geometry) : Void;
-    function createRibbonBuffers(geometry:Geometry) : Void;
-    function createMeshBuffers(geometry:Geometry) : Void;
+    //function createParticleBuffers(geometry:Geometry) : Void;
+    //function createLineBuffers(geometry:Geometry) : Void;
+    //function createRibbonBuffers(geometry:Geometry) : Void;
+    //function createMeshBuffers(geometry:Geometry) : Void;
 
-    function renderBufferImmediate(object:Object3D, program:Dynamic, material:Material) : Void;
-    function renderBufferDirect(camera:Camera, lights:Array<Light>, fog:Fog, material:Material, geometry:Geometry, object:Object3D) : Void;
+    function renderBufferImmediate( object : Object3D, program : Dynamic ) : Void;
+    function renderBufferDirect( camera : Camera, scene : Scene, geometry : Geometry, material : Material, object : Object3D, group : Dynamic ) : Void;
+   
     function setAnimationLoop( callback : Void->Void ) : Void;
 
     @:overload(function( scene:Scene, camera:Camera, ?renderTarget:WebGLRenderTarget, ?forceClear:Bool ):Void{})
