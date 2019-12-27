164869965efc24686544ef141d22bdbb203f37a4
tong <tong@disktree.net>
2019-12-27 16:31:50 +0100

    update webxr extern

--- src/three/renderers/webxr/WebXRManager.hx
+++ src/three/renderers/webxr/WebXRManager.hx
@@ -1,24 +1,23 @@
 package three.renderers.webxr;
 
+import js.html.webgl.RenderingContext;
+import three.cameras.ArrayCamera;
 import three.cameras.Camera;
-import three.math.Matrix4;
-
-//TODO
+import three.objects.Group;
 
 @:native("THREE.WebXRManager")
 extern class WebXRManager {
-
     var enabled : Bool;
     var isPresenting : Bool;
-
-    function new( renderer : WebGLRenderer ) : Void;
-
-    function getDevice() : Dynamic;
-    function setDevice( value : Dynamic ) : Void;
-    function setSession( value : Dynamic, options : Dynamic ) : Void;
-    function updateCamera( camera : Camera, ?parent : Dynamic ) : Void;
-    function getCamera( camera : three.cameras.Camera ) : three.cameras.ArrayCamera;
+    function new( renderer : WebGLRenderer, gl : RenderingContext ) : Void;
+    function getController( id : Int) : Group;
+    //TODO: empty js //function setFramebufferScaleFactor() : Void;
+    function setFramebufferScaleFactor( value : Float ) : Void;
+    function setReferenceSpaceType( value : Float ) : Void;
+    function getReferenceSpace() : Dynamic; //TODO:
+    function getSession() : Dynamic; //TODO:
+    function setSession( value : Dynamic ) : Void;
+    function getCamera( camera : Camera ) : ArrayCamera;
     function setAnimationLoop( callback : Void->Void ) : Void;
     function dispose() : Void;
-    function getStandingMatrix() : Matrix4;
 }
