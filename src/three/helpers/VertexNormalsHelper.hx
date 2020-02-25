1b8e332711e114e1c60e1d9c6fde722c4e4be176
tong <tong@disktree.net>
2020-02-25 16:10:04 +0100

    @:deprecated helpers

--- src/three/helpers/VertexNormalsHelper.hx
+++ src/three/helpers/VertexNormalsHelper.hx
@@ -3,6 +3,7 @@ package three.helpers;
 import three.objects.Mesh;
 import three.objects.Line;
 
+@:deprecated
 @:native("THREE.VertexNormalsHelper")
 extern class VertexNormalsHelper extends Line {
 	function new( object : Mesh, ?size : Float, ?hex : Int, ?linewidth : Float ) : Void;
--- src/three/helpers/VertexTangentsHelper.hx
+++ src/three/helpers/VertexTangentsHelper.hx
@@ -3,6 +3,7 @@ package three.helpers;
 import three.objects.Mesh;
 import three.objects.Line;
 
+@:deprecated
 @:native("THREE.VertexTangentsHelper")
 extern class VertexTangentsHelper extends three.objects.Line {
 	function new( object : Mesh, ?size : Float, ?hex : Int, ?linewidth : Float ) : Void;
