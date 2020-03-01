443d9b6273b37013b675cd01d972c9ccfc3d2b25
tong <tong@disktree.net>
2020-03-01 01:31:20 +0100

    use haxe4 syntax

--- src/three/Lib.hx
+++ src/three/Lib.hx
@@ -28,43 +28,43 @@ class Lib {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-@:enum abstract CullFace(Int) from Int to Int {
+enum abstract CullFace(Int) from Int to Int {
 	var None = 0;
 	var Back = 1;
 	var Front = 2;
 	var FrontBack = 3;
 }
 
-@:enum abstract FrontFaceDirection(Int) from Int to Int {
+enum abstract FrontFaceDirection(Int) from Int to Int {
 	var CW = 0;
 	var CCW = 1;
 }
 
-@:enum abstract ShadowMapType(Int) from Int to Int {
+enum abstract ShadowMapType(Int) from Int to Int {
 	var BasicShadowMap = 0;
 	var PCFShadowMap = 1;
 	var PCFSoftShadowMap = 2;
 }
 
-@:enum abstract Side(Int) from Int to Int {
+enum abstract Side(Int) from Int to Int {
 	var FrontSide = 0;
 	var BackSide = 1;
 	var DoubleSide = 2;
 }
 
-@:enum abstract Shading(Int) from Int to Int {
+enum abstract Shading(Int) from Int to Int {
 	var NoShading = 0;
 	var FlatShading = 1;
 	var SmoothShading = 2;
 }
 
-@:enum abstract Colors(Int) from Int to Int {
+enum abstract Colors(Int) from Int to Int {
 	var NoColors = 0;
 	var FaceColors = 1;
 	var VertexColors = 2;
 }
 
-@:enum abstract BlendMode(Int) from Int to Int {
+enum abstract BlendMode(Int) from Int to Int {
 	var NoBlending = 0;
 	var NormalBlending = 1;
 	var AdditiveBlending = 2;
@@ -73,7 +73,7 @@ class Lib {
 	var CustomBlending = 5;
 }
 
-@:enum abstract BlendingEquation(Int) from Int to Int {
+enum abstract BlendingEquation(Int) from Int to Int {
 	var AddEquation = 100;
 	var SubtractEquation = 101;
 	var ReverseSubtractEquation = 102;
@@ -81,7 +81,7 @@ class Lib {
 	var MaxEquation = 104;
 }
 
-@:enum abstract BlendingDestinationFactor(Int) from Int to Int {
+enum abstract BlendingDestinationFactor(Int) from Int to Int {
 	var ZeroFactor = 200;
 	var OneFactor = 201;
 	var SrcColorFactor = 202;
@@ -95,7 +95,7 @@ class Lib {
 	var SrcAlphaSaturateFactor = 210;
 }
 
-@:enum abstract DepthModes(Int) from Int to Int {
+enum abstract DepthModes(Int) from Int to Int {
 	var NeverDepth = 0;
 	var AlwaysDepth = 1;
 	var LessDepth = 2;
@@ -106,13 +106,13 @@ class Lib {
 	var NotEqualDepth = 7;
 }
 
-@:enum abstract Combine(Int) from Int to Int {
+enum abstract Combine(Int) from Int to Int {
 	var MultiplyOperation = 0;
 	var MixOperation = 1;
 	var AddOperation = 2;
 }
 
-@:enum abstract ToneMapping(Int) from Int to Int {
+enum abstract ToneMapping(Int) from Int to Int {
 	var NoToneMapping = 0;
 	var LinearToneMapping = 1;
 	var ReinhardToneMapping = 2;
@@ -121,7 +121,7 @@ class Lib {
 	var ACESFilmicToneMapping = 5;
 }
 
-@:enum abstract Mapping(Int) from Int to Int {
+enum abstract Mapping(Int) from Int to Int {
 	var UVMapping = 300;
 	var CubeReflectionMapping = 301;
 	var CubeRefractionMapping = 302;
@@ -132,31 +132,31 @@ class Lib {
 	var CubeUVRefractionMapping = 307;
 }
 
-@:enum abstract TextureConstant(Int) from Int to Int {
+enum abstract TextureConstant(Int) from Int to Int {
 	var MultiplyOperation = 0;
 	var MixOperation = 1;
 	var AddOperation = 2;
 }
 
-@:enum abstract DrawMode(Int) from Int to Int {
+enum abstract DrawMode(Int) from Int to Int {
 	var TrianglesDrawMode = 0;
 	var TriangleStripDrawMode = 1;
 	var TriangleFanDrawMode = 2;
 }
 
 // From src/objects/Line.js
-@:enum abstract LineType(Int) from Int to Int {
+enum abstract LineType(Int) from Int to Int {
 	var LineStrip = 0;
 	var LinePieces = 1;
 }
 
-@:enum abstract WrappingMode(Int) from Int to Int {
+enum abstract WrappingMode(Int) from Int to Int {
 	var RepeatWrapping = 1000;
 	var ClampToEdgeWrapping = 1001;
 	var MirroredRepeatWrapping = 1002;
 }
 
-@:enum abstract TextureFilter(Int) from Int to Int {
+enum abstract TextureFilter(Int) from Int to Int {
 	var NearestFilter = 1003;
 	var NearestMipMapNearestFilter = 1004;
 	var NearestMipMapLinearFilter = 1005;
@@ -165,7 +165,7 @@ class Lib {
 	var LinearMipMapLinearFilter = 1008;
 }
 
-@:enum abstract TextureDataType(Int) from Int to Int {
+enum abstract TextureDataType(Int) from Int to Int {
 	var UnsignedByteType = 1009;
 	var ByteType = 1010;
 	var ShortType = 1011;
@@ -176,14 +176,14 @@ class Lib {
 	var HalfFloatType = 1016;
 }
 
-@:enum abstract PixelType(Int) from Int to Int {
+enum abstract PixelType(Int) from Int to Int {
 	var UnsignedShort4444Type = 1017;
 	var UnsignedShort5551Type = 1018;
 	var UnsignedShort565Type = 1019;
 	var UnsignedInt248Type = 1020;
 }
 
-@:enum abstract PixelFormat(Int) from Int to Int {
+enum abstract PixelFormat(Int) from Int to Int {
 	var AlphaFormat = 1021;
 	var RGBFormat = 1022;
 	var RGBAFormat = 1023;
@@ -194,7 +194,7 @@ class Lib {
 	var DepthStencilFormat = 1027;
 }
 
-@:enum abstract TextureFormat(Int) from Int to Int {
+enum abstract TextureFormat(Int) from Int to Int {
 	// DDS / ST3C Compressed texture formats
 	var RGB_S3TC_DXT1_Format = 2001;
 	var RGBA_S3TC_DXT1_Format = 2002;
@@ -207,25 +207,25 @@ class Lib {
 	var RGBA_PVRTC_2BPPV1_Format = 2103;
 }
 
-@:enum abstract LoopMode(Int) from Int to Int {
+enum abstract LoopMode(Int) from Int to Int {
 	var LoopOnce = 2200;
 	var LoopRepeat = 2201;
 	var LoopPingPong = 2202;
 }
 
-@:enum abstract InterpolationModes(Int) from Int to Int {
+enum abstract InterpolationModes(Int) from Int to Int {
 	var InterpolateDiscrete = 2300;
 	var InterpolateLinear = 2301;
 	var InterpolateSmooth = 2302;
 }
 
-@:enum abstract Ending(Int) from Int to Int {
+enum abstract Ending(Int) from Int to Int {
 	var ZeroCurvatureEnding = 2400;
 	var ZeroSlopeEnding = 2401;
 	var WrapAroundEnding = 2402;
 }
 
-@:enum abstract TextureEncoding(Int) from Int to Int {
+enum abstract TextureEncoding(Int) from Int to Int {
 	var LinearEncoding = 3000;
 	var sRGBEncoding = 3001;
 	var GammaEncoding = 3007;
@@ -236,12 +236,12 @@ class Lib {
 	var RGBDEncoding = 3006;
 }
 
-@:enum abstract DepthPacking(Int) from Int to Int {
+enum abstract DepthPacking(Int) from Int to Int {
 	var BasicDepthPacking = 3200;
 	var RGBADepthPacking = 3201;
 }
 
-@:enum abstract StencilOp(Int) from Int to Int {
+enum abstract StencilOp(Int) from Int to Int {
 	var ZeroStencilOp = 0;
 	var KeepStencilOp = 7680;
 	var ReplaceStencilOp = 7681;
@@ -252,7 +252,7 @@ class Lib {
 	var InvertStencilOp = 5386;
 }
 
-@:enum abstract StencilFunc(Int) from Int to Int {
+enum abstract StencilFunc(Int) from Int to Int {
 	var NeverStencilFunc = 512;
 	var LessStencilFunc = 513;
 	var EqualStencilFunc = 514;
@@ -263,7 +263,7 @@ class Lib {
 	var AlwaysStencilFunc = 519;
 }
 
-@:enum abstract Mouse(Int) from Int to Int {
+enum abstract Mouse(Int) from Int to Int {
 	var Left = 0;
 	var Middle = 1;
 	var Right = 2;
@@ -272,7 +272,7 @@ class Lib {
 	var Pan = 2;
 }
 
-@:enum abstract Usage(Int) from Int to Int {
+enum abstract Usage(Int) from Int to Int {
 	var StaticDrawUsage = 35044;
 	var DynamicDrawUsage = 35048;
 	var StreamDrawUsage = 35040;
