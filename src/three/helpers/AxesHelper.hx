c8574c2d62cafbae7df3b078d8c127fcf5612da3
tong <tong@disktree.net>
2021-06-30 19:10:13 +0200

    Upgrade to three.js r130

--- src/three/helpers/AxesHelper.hx
+++ src/three/helpers/AxesHelper.hx
@@ -1,9 +1,11 @@
 package three.helpers;
 
+import three.math.Color;
 import three.objects.LineSegments;
 
 #if three_jsRequire @:jsRequire("three", "AxesHelper") #else @:native("THREE.AxesHelper") #end
 extern class AxesHelper extends LineSegments {
 	function new( ?size : Float ) : Void;
+	function setColors( xAxisColor : Color, yAxisColor : Color, zAxisColor : Color ) : Void;
 	function dispose() : Void;
 }
